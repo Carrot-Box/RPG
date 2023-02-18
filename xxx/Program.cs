d89434f13a28a38bf1d843e6050696d7a66fcaf2
Carrot-Box <rjh7907@naver.com>
2023-02-19 00:32:48 +0900

    버그수정,레벨업,전투종료후 선택지추가

--- xxx/Program.cs
+++ xxx/Program.cs
@@ -1,56 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+//시스템 추가 예정
+//나나나나ㅏ나중 최종 시스템
+//아군공격력 100 - 적군방어력 50 = +50
+//아군공격력 100 - 적군방어력 200 = -100
+//최소 5%의 딜 보정이 되어서 5%의 딜이 들간다. -> 명방 방어력 메커니즘
+//-------------------
+
+//0. 전투종료후 마을 or Next선택지 만들기 (O)
+//1. LVUP기능 (공격력 강화 체력증가 방어력증가) (0)
+//2. 방어력 먼저 깍고 체력 깎는 시스템
+//3. 다른 스테이지에 다른 몬스터 나오기
 class FightUnit
 {
-    protected string Name = "none";
-    protected int AT = 200;
+    protected string Name = "None";
+    protected int ATT = 200;
+    protected int DEF = 100;
     protected int HP = 500;
     protected int MAXHP = 1000;
-    public void StatusRender()
-    {
-        Console.Write(Name);
-        Console.WriteLine("-------------------");
-        Console.WriteLine("AT : " + AT);
-        Console.Write("HP : " + HP);
-        Console.WriteLine("/" + MAXHP);
-        Console.WriteLine("--------------------------");
-    }
+
+    // bool : 논리형 내가 HP가 0이나 아니냐를 알려주는것(참과 거짓)
     public bool IsDeath()
     {
+        // bool boolDeath = HP <= 0;
+        // 연산자도 함수에 가까움, 리턴값이(결과값)이 있다
         return HP <= 0;
     }
     public void Damage(FightUnit _OtherUnit)
     {
         Console.Write(Name);
-        Console.Write("가" + _OtherUnit.AT);
-        Console.WriteLine("의 데미지를 입혔습니다.");
+        Console.Write("가" + _OtherUnit.ATT);
+        Console.WriteLine("의 데미지를 입었습니다");
+        Console.ReadKey();
+        HP -= _OtherUnit.ATT;
+    }
+    public void StatusRender()
+    {
+        Console.Write(Name);
+        Console.WriteLine("------------------------");
+        Console.Write("AT : " + ATT);
+        Console.WriteLine("         DEF : " + DEF);
+        Console.Write("HP : " + HP);
+        Console.WriteLine("/" + MAXHP);
+        Console.WriteLine("------------------------------");
+    }
+    public void LvUp()
+    {
+        ATT *= 2;
+        DEF *= 2;
+        HP *= 2;
+        MAXHP *= 2;
+        StatusRender();
+        Console.WriteLine("플레이어가 레벨업 하였습니다.");
+    }
+    public void AttMechanism()
+    {
+        if (true)
+        {
+            // 만약 Def가 0이 아니라면 Def를 떄린다
+            // 만약 Def가 0이라면 Hp를 공격한다.
+        }
     }
 }
-
 class Player : FightUnit
 {
+    public Player(string _Name)
+    {
+        Name = _Name;
+    }
     public void PrintHp()
     {
-        Console.Write("현재 당신의 HP는 " + HP);
+        Console.Write("현재 플레이어의 HP는" + HP);
         Console.WriteLine("입니다.");
+        Console.ReadKey();
     }
-    public void MaxHp()
+    public void MaxHeal(/*Player this*/)
     {
+        // 작은 함수는 많이많이 만들자.
         if (HP < MAXHP)
         {
-            HP = MAXHP;
             PrintHp();
+            this.HP = MAXHP;
         }
         else
         {
-            Console.WriteLine("체력이 가득찼습니다.");
+            Console.Write("현재 플레이어의 HP는" + HP);
+            Console.WriteLine("입니다.");
+            Console.ReadKey();
+            Console.WriteLine("플레이어의 체력이 가득찼습니다.");
+            Console.ReadKey();
         }
     }
-
 }
 class Monster : FightUnit
 {
@@ -59,123 +104,151 @@ class Monster : FightUnit
         Name = _Name;
     }
 }
+class Stage
+{
+    // 스테이지는 나아아아아중에 하자
+    // 몬스터 정보 / 몬스터는 스펙up
+}
 enum STARTSELECT
 {
-    TOWN,
-    BATTLE,
-    NONE
+    SELECTTOWN,
+    SELECTBATTLE,
+    NONESELECT
 }
-namespace xxx
+namespace TEXTRPG
 {
     internal class Program
     {
         static STARTSELECT StartSelect()
         {
             Console.Clear();
-            Console.WriteLine("이세계에 오신걸 환영합니다. \n어디로 가시겠습니까?");
-            Console.WriteLine("1. 마을");
-            Console.WriteLine("2. 전장");
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("이세계에 오신것을 환영합니다,\n어느곳으로 가실 겁니까?");
+            Console.WriteLine("1. 마을 \n2. 배틀");
+            Console.WriteLine("------------------------------");
 
             ConsoleKeyInfo CKI = Console.ReadKey();
             switch (CKI.Key)
             {
                 case ConsoleKey.D1:
-                    Console.WriteLine("마을에 입장하시겠습니다.");
+                    Console.WriteLine("마을로 떠나겠습니다.");
                     Console.ReadKey();
-                    return STARTSELECT.TOWN;
+                    return STARTSELECT.SELECTTOWN;
                 case ConsoleKey.D2:
-                    Console.WriteLine("전장으로 입장하셨습니다.");
+                    Console.WriteLine("전장으로 떠나겠습니다.");
                     Console.ReadKey();
-                    return STARTSELECT.BATTLE;
+                    return STARTSELECT.SELECTBATTLE;
                 default:
-                    Console.WriteLine("잘못된 선택입니다");
+                    Console.WriteLine("잘못 입력하였습니다.");
                     Console.ReadKey();
-                    return STARTSELECT.NONE;
+                    return STARTSELECT.NONESELECT;
             }
         }
-        static void Town(Player _Player)
-        {   
+        static STARTSELECT Town(Player _Player)
+        {
             while (true)
-            {
+            {// 탈주 코드
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+                // 탈주 
+
                 Console.Clear();
                 _Player.StatusRender();
-                Console.WriteLine("마을에 입장하신걸 환영합니다. \n무엇을 고르겠습니까");
-                Console.WriteLine("1. 체력회복");
-                Console.WriteLine("2. 머하지..?");
-                Console.WriteLine("3. 나가기");
-                Console.WriteLine("----------------------------");
+                Console.WriteLine("마을에 오신것을 환영합니다. \n무엇을 도와드릴까요?");
+                Console.WriteLine("1. 체력회복 \n2. 머하지... \n3. 떠나기");
+                Console.WriteLine("------------------------------");
 
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.D1:
-                        _Player.MaxHp();
-                        Console.ReadKey();
+                        _Player.MaxHeal();
                         break;
                     case ConsoleKey.D2:
-                        // 미정
+                        Console.WriteLine("이것을 보고있는 당신 생각해봐!");
+                        Console.ReadKey();
                         break;
                     case ConsoleKey.D3:
-                        return;
+                        return STARTSELECT.NONESELECT;
                 }
             }
         }
-        static void Battle(Player _Player)
+        static STARTSELECT Battle(Player _Player)
         {
             Monster NewMonster = new Monster("오크");
-
-            while (false == NewMonster.IsDeath() && false == _Player.IsDeath())
+            while (!NewMonster.IsDeath() && !_Player.IsDeath())
             {
                 Console.Clear();
                 _Player.StatusRender();
                 NewMonster.StatusRender();
-                Console.WriteLine("몬스터가 출현했다!!! \n무엇을 고르겠습니까");
-                Console.WriteLine("1. 싸운다");
-                Console.WriteLine("2. 자폭한다");
-                Console.WriteLine("3. 도망간다");
-                Console.WriteLine("----------------------------");
-                //Console.ReadKey();
-
+                Console.ReadKey();
                 switch (Console.ReadKey().Key)
                 {
                     case ConsoleKey.D1:
-                        Console.ReadKey();
+                        Console.WriteLine("");
+                        // 둘다 죽는걸 방지하기 위해서 한대 떄리자마자 채크
+                        // 그걸 if로 묶어주고 뉴몬스터가 안죽었을때만 플레이어가 대미지를 줄 수 있도록
+
                         NewMonster.Damage(_Player);
-                        if (false == NewMonster.IsDeath())
+                        if (!NewMonster.IsDeath())
                         {
                             _Player.Damage(NewMonster);
-                            //NewMonster.MonStatusRender();
                         }
-                        // 오류 체력마이너스가 안돌아감 수정희망
-                        Console.WriteLine("승리하였습니다.\n마을로 돌아가겠습니다.");
-                        Console.ReadKey();
+                        else
+                        {
+                            // 몬스터가 죽었을 경우
+                            NewMonster.IsDeath();
+                            Console.WriteLine("몬스터 체력이 0이 되었습니다.");
+                            Console.ReadKey();
+                            Console.Clear();
+                            Console.WriteLine("전투에서 승리하였습니다");
+                            _Player.LvUp();
+                            Console.WriteLine("어디로 가시겠습니까?");
+                            Console.WriteLine("1. 마을로 돌아가기\n2. 다음 스테이지로 넘어가지");
+                            Console.WriteLine("------------------------------");
+                            switch (Console.ReadKey().Key)
+                            {
+                                case ConsoleKey.D1:
+                                    Console.WriteLine("마을로 돌아가겠습니다.");
+                                    Console.ReadKey();
+                                    return STARTSELECT.SELECTTOWN;
+                                case ConsoleKey.D2:
+                                    Console.WriteLine("다음 스테이지가 없습니다. 마을로 돌아가겠습니다.");
+                                    Console.ReadKey();
+                                    return STARTSELECT.SELECTTOWN;
+                            }
+                        }
                         break;
-                    case ConsoleKey.D2:
-                        Console.WriteLine("펑!");
-                        Console.ReadKey();
-                        return;
-                    case ConsoleKey.D3:
-                        Console.WriteLine("니게룽다요~");
-                        Console.ReadKey();
-                        return;
                 }
             }
+            return STARTSELECT.SELECTTOWN;
         }
         static void Main(string[] args)
         {
-            Player NewPlayer = new Player();
+            // 반복문에안들어가도록 반드시 조심할것
+            Player NewPlayer = new Player("플레이어");
+
+            STARTSELECT SelectCheck = STARTSELECT.NONESELECT;
 
             while (true)
             {
-                STARTSELECT SelectCheck = StartSelect();
-
                 switch (SelectCheck)
                 {
-                    case STARTSELECT.TOWN:
-                        Town(NewPlayer);
+                    case STARTSELECT.NONESELECT:
+                        SelectCheck = StartSelect();
                         break;
-                    case STARTSELECT.BATTLE:
-                        Battle(NewPlayer);
+                    case STARTSELECT.SELECTTOWN:
+                        SelectCheck = Town(NewPlayer);
+                        break;
+                    case STARTSELECT.SELECTBATTLE:
+                        SelectCheck = Battle(NewPlayer);
                         break;
                 }
             }
@@ -183,4 +256,4 @@ namespace xxx
     }
 }
 
-// 2.16 몬스터 클래스 + 니게룽다요
+//
